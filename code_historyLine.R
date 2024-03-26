bf83ff523ba0ab50d768ec969c2da4cfd5dacbb6
BERN340\Student <lars.wenger@students.bfh.ch>
2024-03-26 10:28:38 +0100

    Update historyLine.Rmd

--- code_historyLine.R
+++ code_historyLine.R
@@ -16,12 +16,16 @@ history_data <- read_csv("Data/sys_history_line.csv", locale = locale(encoding =
 data <- history_data
 
 data$set <- trimws(gsub("Case:", " ", data$set))
+
 data$update_time <- as.POSIXct(strptime(data$update_time, "%d-%m-%Y %H:%M:%S", tz="UTC"))
 
 data_all <- read_csv("Data/data.csv", locale = locale(encoding = "UTF-8"))
 #Merge opened and closed from data_new to data by number
 data_all <- select(data_all, number, account, opened, closed, openedToClosed)
 data <- merge(data, data_all, by.x = "set", by.y = "number", all.x = FALSE)
+
+# Remove all entries where update_time after closed_at. Because Flows can Change fields after they are closed
+data <- data %>% filter(update_time <= closed)
 
 # Add Column for the time difference between opened_at and update_time
 data <- data %>%
@@ -31,39 +35,44 @@ data <- data %>%
 
 data_state <- data %>%
   filter(field == "state") %>%
+  filter(!is.na(new)) %>%
   group_by(set) %>%
   arrange(update_time) %>%
   mutate(time_diff_previous_state = (as.numeric(update_time - lag(update_time))/3600)) %>%
   mutate(sum_time_diff_state = sum(time_diff_previous_state, na.rm = TRUE)) %>%
   mutate(percentage_time_diff_state = time_diff_previous_state / sum_time_diff_state * 100) %>%
+  select(set, new, percentage_time_diff_state) %>%
   ungroup()
 
 data_assignment_group <- data %>%
   filter(field == "assignment_group") %>%
+  filter(!is.na(new)) %>%
   group_by(set) %>%
   arrange(update_time) %>%
   mutate(time_diff_previous_assignment_group = (as.numeric(update_time - lag(update_time))/3600)) %>%
   mutate(sum_time_diff_assignment_group = sum(time_diff_previous_assignment_group, na.rm = TRUE)) %>%
   mutate(percentage_time_diff_assignment_group = time_diff_previous_assignment_group / sum_time_diff_assignment_group * 100) %>%
+  select(set, new, percentage_time_diff_assignment_group) %>%
   ungroup()
 
 data_assigned_to <- data %>%
   filter(field == "assigned_to") %>%
+  filter(!is.na(new)) %>%
   group_by(set) %>%
   arrange(update_time) %>%
   mutate(time_diff_previous_assigned_to = (as.numeric(update_time - lag(update_time))/3600)) %>%
   mutate(sum_time_diff_assigned_to = sum(time_diff_previous_assigned_to, na.rm = TRUE)) %>%
   mutate(percentage_time_diff_assigned_to = time_diff_previous_assigned_to / sum_time_diff_assigned_to * 100) %>%
+  select(set, new, percentage_time_diff_assigned_to) %>%
   ungroup()
 
 
 
 
 
-# Remove all entries where update_time after closed_at
-data <- data %>% filter(update_time <= closed)
+
 #List of field values that will be filtered
-field_values <- c("state", "assignment_group")
+field_values <- c("state", "assignment_group", "assigned_to")
 
 # Only remain rows where field value is in field_values
 data <- data %>% filter(field %in% field_values)
@@ -114,106 +123,50 @@ one_hot_encode <- function(df, columns) {
   return(cbind(df[1], result_df))
 }
 # One-hot encoding
-encoded_df <- one_hot_encode(data, c("fields"))
+# encoded_df <- one_hot_encode(data, c("fields"))
+encoded_df_state <- one_hot_encode(data_state, c("new"))
+encoded_df_assignmet_group <- one_hot_encode(data_assignment_group, c("new"))
+encoded_df_assigned_to <- one_hot_encode(data_assigned_to, c("new"))
 
-#replace all 1 in encode_df[23:46] with the time_diff_previous
-encoded_df[23:46] <- encoded_df[23:46] * encoded_df$time_diff_previous
+#remove second column
+encoded_df_state <- encoded_df_state[,-2]
+encoded_df_assignmet_group <- encoded_df_assignmet_group[,-2]
+encoded_df_assigned_to <- encoded_df_assigned_to[,-2]
 
+#replace all 1 with the time_diff_previous
+encoded_df_state[,c(-1,-2)] <- encoded_df_state[,c(-1,-2)] * encoded_df_state$percentage_time_diff_state
+encoded_df_assignmet_group[,c(-1,-2)] <- encoded_df_assignmet_group[,c(-1,-2)] * encoded_df_assignmet_group$percentage_time_diff_assignment_group
+encoded_df_assigned_to[,c(-1,-2)] <- encoded_df_assigned_to[,c(-1,-2)] * encoded_df_assigned_to$percentage_time_diff_assigned_to
 
+#Round to 2 decimal places
+encoded_df_state[,-1] <- round(encoded_df_state[,-1], 2)
+encoded_df_assignmet_group[,-1] <- round(encoded_df_assignmet_group[,-1], 2)
+encoded_df_assigned_to[,-1] <- round(encoded_df_assigned_to[,-1], 2)
 
+#Group by set and sum all columns
+grouped_encoded_df_state <- encoded_df_state %>%
+  group_by(set) %>%
+  summarise(across(c(-1), sum, na.rm = TRUE)) %>%
+  ungroup()
 
+grouped_encoded_df_assignmet_group <- encoded_df_assignmet_group %>%
+  group_by(set) %>%
+  summarise(across(c(-1), sum, na.rm = TRUE)) %>%
+  ungroup()
 
-data <- select(data, set, opened_at, closed_at, update_time,time_diff_opened,time_diff_previous, user_id, field, old, new)
+grouped_encoded_df_assigned_to <- encoded_df_assigned_to %>%
+  group_by(set) %>%
+  summarise(across(c(-1), sum, na.rm = TRUE)) %>%
+  ungroup()
 
+#Replace all columns header new with "percentage"
+colnames(grouped_encoded_df_state) <- gsub("new", "prop_state_", colnames(grouped_encoded_df_state))
+colnames(grouped_encoded_df_assignmet_group) <- gsub("new", "prop_group_", colnames(grouped_encoded_df_assignmet_group))
+colnames(grouped_encoded_df_assigned_to) <- gsub("new", "prop_user_", colnames(grouped_encoded_df_assigned_to))
 
-data_ts <- data %>%
-  distinct(set, update_time, field, .keep_all = TRUE) %>%
-  pivot_wider(
-    names_from = field, 
-    values_from = c(old, new),
-    names_glue = "{field}_{.value}"
-  ) %>%
-  select(update_time, set, user_id, sort(tidyselect::peek_vars()))
+#Merge all dataframes
+final_df <- merge(grouped_encoded_df_state, grouped_encoded_df_assignmet_group, by = "set", all = TRUE)
+final_df <- merge(final_df, grouped_encoded_df_assigned_to, by = "set", all = TRUE)
 
-
-
-# data_ts <- data_ts %>% arrange(set, update_time) %>%
-#   # Group by set to ensure filling is done within each set
-#   group_by(set) %>%
-#   fill(state_new, assignment_group_new, assigned_to_new, .direction = "downup") %>%
-#   # Fill NA values in the new columns with the old values from the previous row
-#   # Ungroup to avoid unexpected grouping behavior later
-#   ungroup()
-
-print(data_ts)
-
-outlier <- function(x){
-  quantiles <- quantile(x, c(.05, .95))
-  x[x < quantiles[1]] <- quantiles[1]
-  x[x > quantiles[2]] <- quantiles[2]
-  x
-}
-
-#if time_diff_previous is NA, then set it to 0
-data_ts$time_diff_previous[is.na(data_ts$time_diff_previous)] <- 0
-
-
-#Use function outlier for the dataset
-data_ts['time_diff_previous'] <- map_df(data_ts['time_diff_previous'], outlier)
-data_ts['time_diff_opened'] <- map_df(data_ts['time_diff_opened'], outlier)
-
-# Gerenate a time series plot
-data_ts %>%
-  ggplot(aes(x = update_time, y = time_diff_previous)) +
-  geom_line() +
-  labs(title = "Time difference between opened_at and update_time",
-       x = "Time",
-       y = "Time difference (in seconds)") +
-  theme_minimal()
-
-
-# Plot timeline
-data_ts %>%
-  filter(!is.na(assignment_group_old)) %>%
-  ggplot(aes(x = update_time, y = assignment_group_new)) +
-  geom_point() +
-  labs(title = "Timeline of updates per group",
-       x = "Time",
-       y = "Group") +
-  theme_minimal()
-
-# Plot Histogramm of time_diff_previous
-data_ts %>%
-  ggplot(aes(x = time_diff_previous)) +
-  geom_histogram(binwidth = 10) +
-  labs(title = "Histogram of time difference between update_time and the previous update_time",
-       x = "Time Since Last Action (in seconds)",
-       y = "Frequency") +
-  theme_minimal()
-
-# Analyze only when the assignment_group_old is not NA. Plot mean_diff_previous per Changes from assignment_group
-data_ts %>%
-  filter(!is.na(assignment_group_new)) %>%
-  group_by(assignment_group_new) %>%
-  filter(n() > 10) %>%
-  summarise(mean_time_diff_previous = mean(time_diff_previous, na.rm = TRUE)) %>%
-  ggplot(aes(x = assignment_group_new, y = mean_time_diff_previous)) +
-  geom_col() +
-  labs(title = "Mean time difference between update_time and the previous update_time per assignment_group",
-       x = "Assignment Group",
-       y = "Mean time difference (in seconds)") +
-  theme_minimal()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-data_ts %>%
-  filter(!is.na(assigned_to_old)) %>%
-  group_by(assigned_to_new) %>%
-  summarise(mean_time_diff_previous = mean(time_diff_previous, na.rm = TRUE)) %>%
-  ggplot(aes(x = assigned_to_new, y = mean_time_diff_previous)) +
-  geom_col() +
-  labs(title = "Mean time difference between update_time and the previous update_time per assignment_group",
-       x = "Assigned to",
-       y = "Mean time difference (in seconds)") +
-  theme_minimal()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+#Rename set to number
+colnames(final_df)[1] <- "number"
